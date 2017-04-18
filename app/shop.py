7b250f950ef6baa97b1259ebcf7238ac84e41f58
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-18 19:31:58 +0300

    Adds add_stock() which in turn uses the stock update_stock() to update the stock of the shop.

--- app/shop.py
+++ app/shop.py
@@ -1,5 +1,13 @@
+from app.stock import Stock
+
+
 class Shop(object):
     def __init__(self, name):
         self.name = name
+        self.stock = Stock()
+
+    def add_stock(self, item, quantity):
+        self.stock.update_stock(item, quantity)
+
 
 
