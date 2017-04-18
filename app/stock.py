37c1150982932cda85e93fe5745968d646e16fb9
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-18 16:37:54 +0300

    Adds stock model to manage the Shop's stock

--- app/stock.py
+++ app/stock.py
@@ -1,11 +1,14 @@
-class Stock:
+class Stock(object):
 
     def __init__(self):
         self.stock_items = {}
 
     def update_stock(self, item, quantity):
         if self.stock_items.has_key(item.name):
-            self.stock_items[item.name] += quantity
+            self.stock_items[item.name]['quantity'] += quantity
         else:
-            self.stock_items[item.name] = quantity
+            self.stock_items[item.name] = {'name': item.name, 'unit_price': item.unit_price, 'quantity': quantity}
 
+    def get_sub_total(self, item):
+        item_unit_price = self.stock_items[item.name]['unit_price']
+        return item.quantity * item_unit_price
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,6 +1,6 @@
 import unittest
 from app.stock import Stock
-from app.item import Item
+from app.item import StockItem
 
 
 class TestStock(unittest.TestCase):
@@ -8,18 +8,24 @@ class TestStock(unittest.TestCase):
         self.stock = Stock()
 
     def test_stock_adds_new_stock_items(self):
-        item = Item('Fresh Diary Milk', 1000)
+        item = StockItem('Fresh Diary Milk', 1000)
         self.stock.update_stock(item, 20)
-        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk': 20})
+        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk':
+                                                      {'name': 'Fresh Diary Milk', 'quantity': 20, 'unit_price': 1000}})
 
     def test_stock_adds_multiple_stock_items(self):
-        items = [Item('Fresh Diary Milk', 1000), Item('Bic Pen', 40)]
+        items = [StockItem('Fresh Diary Milk', 1000), StockItem('Bic Pen', 2000)]
         for item in items:
             self.stock.update_stock(item, 20)
-        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk': 20, 'Bic Pen': 20})
+        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk':
+                                                      {'name': 'Fresh Diary Milk', 'quantity': 20, 'unit_price': 1000},
+                                                  'Bic Pen':
+                                                      {'name': 'Bic Pen', 'quantity': 20, 'unit_price': 2000}
+                                                  })
 
     def test_stock_updates_existing_stock_item(self):
-        item = Item('Fresh Diary Milk', 1000)
+        item = StockItem('Fresh Diary Milk', 1000)
         self.stock.update_stock(item, 20)
         self.stock.update_stock(item, 40)
-        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk': 60})
+        self.assertEqual(self.stock.stock_items, {'Fresh Diary Milk':
+                                                      {'name': 'Fresh Diary Milk', 'quantity': 60, 'unit_price': 1000}})
