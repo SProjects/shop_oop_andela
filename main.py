09adab6e1dcbf365f6961ab0c87773eb8b8fa104
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-18 19:33:17 +0300

    Adds code to main.py to ochestrate a user scenario.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from app.shop import Shop
+from app.item import StockItem
+from app.customer import Customer
+from app.shopping_cart import ShoppingCart
+
+
 def main():
-    pass
+    shop = Shop('Nakumatt')
+    stock_shop(shop)
+
+    cart = ShoppingCart()
+    customer = Customer('Daniel Sebuuma', cart)
+    customer.add_to_cart('Blueband', 5)
+    customer.add_to_cart('Toothpaste', 2)
+    print "Shopping Cart contains:: {}".format(cart)
+
+    print customer.check_out(shop)
+
+
+def stock_shop(shop):
+    items = [StockItem('Blueband', 4000), StockItem('Toothpaste', 3000)]
+    for item in items:
+        shop.add_stock(item, 100)
 
 if __name__ == '__main__':
     main()
