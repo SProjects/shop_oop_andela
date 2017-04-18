0b932a96b98acd9047cbb9e6358bfe0446b187ab
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-18 19:29:21 +0300

    Adds add_to_cart(). Adds ShoppingCart to customer concerns since it makes sense that a customer get a cart and not the other way round. Customer is now the one that checks_out. Tests add_to_cart().

--- app/customer.py
+++ app/customer.py
@@ -1,8 +1,17 @@
-class Customer:
-    def __init__(self, name):
-        self.name = name
+from item import LineItem
 
-    def check_out(self, cart):
-        return "{0} has paid UGX: {1}".format(self.name, cart.total_cost())
+
+class Customer:
+    def __init__(self, name, cart):
+        self.name = name
+        self.cart = cart
+
+    def check_out(self, shop):
+        return "{0} has paid UGX: {1}".format(self.name, self.cart.total_cost(shop))
+
+    def add_to_cart(self, item_name, quantity):
+        line_item = LineItem(item_name, quantity)
+        self.cart.add_item(line_item)
+        return self.cart
 
 
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,9 +1,24 @@
 import unittest
+from app.shop import Shop
 from app.customer import Customer
+from app.shopping_cart import ShoppingCart
+from app.item import StockItem
+from app.item import LineItem
 
 
 class TestCustomer(unittest.TestCase):
+    def setUp(self):
+        shop = Shop('Nakumatt')
+        shop.add_stock(StockItem('Blueband', 4000), 100)
 
-    def test_customer_responds_to_name_property(self):
-        customer = Customer('Mukasa John')
-        self.assertEqual(customer.name, 'Mukasa John')
+        cart = ShoppingCart()
+        self.customer = Customer('Mukasa John', cart)
+
+    def test_customer_responds_to_its_properties(self):
+        self.assertEqual(self.customer.name, 'Mukasa John')
+
+    def test_customer_add_item_to_cart(self):
+        self.customer.add_to_cart('Blueband', 3)
+        self.assertEqual(self.customer.cart.line_items, [LineItem('Blueband', 3)])
+
+
