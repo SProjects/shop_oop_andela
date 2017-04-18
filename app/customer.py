15e7a0634dcf32b05ae0c7a1a0056d634561f96a
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-18 16:42:30 +0300

    Refactors to move check_out feature to customer instead of the cart

--- app/customer.py
+++ app/customer.py
@@ -2,4 +2,7 @@ class Customer:
     def __init__(self, name):
         self.name = name
 
+    def check_out(self, cart):
+        return "{0} has paid UGX: {1}".format(self.name, cart.total_cost())
+
 
--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -12,10 +12,7 @@ class ShoppingCart:
     def remove_item(self, line_item):
         self.line_items.remove(line_item)
 
-    def check_out(self):
-        return "{0} has paid UGX: {1}".format(self.customer, self._total_cost())
-
-    def _total_cost(self):
+    def total_cost(self):
         total = 0
         for line_item in self.line_items:
             total += Stock().get_sub_total(line_item)
